a5933547116275e5779537e34b41b216f3c44d04
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-04-20 15:15:04 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+// import Button from './components/button';
+// import Button from '@mui/material/Button';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary'>Hello World</Button>
+      <Button type='default'>Hello World</Button>
+      <Button type='dashed'>Hello World</Button>
+      <div>
+        <Button type='text'>Hello World</Button>
+        <Button type='link'>Hello World</Button>
+      </div>
+
     </div>
   );
 }
